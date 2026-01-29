ac0191bb48241209cc5de6c018d142602965eb9d
yabasskabore <yacouba.abass@rades.fr>
2026-01-29 13:26:53 +0100

    fix drop dependancy bug: use of truncate instead of drop

--- sql/transformations.sql
+++ sql/transformations.sql
@@ -1,8 +1,26 @@
+BEGIN;
+
 -- Create clean table
 
-DROP TABLE IF EXISTS transactions_clean;
+CREATE TABLE IF NOT EXISTS transactions_clean (
+    transaction_id BIGINT,
+    user_id BIGINT,
+    amount NUMERIC,
+    currency TEXT,
+    status TEXT,
+    event_time TIMESTAMP,
+    processing_time TIMESTAMP,
+    event_date DATE,
+    event_hour TIMESTAMP,
+    latency_seconds DOUBLE PRECISION,
+    is_late BOOLEAN
+);
 
-CREATE TABLE transactions_clean AS
+-- Clear data
+TRUNCATE TABLE transactions_clean;
+
+-- Refill table
+INSERT INTO transactions_clean 
 SELECT
     transaction_id,
     user_id,
@@ -34,11 +52,13 @@ WHERE
 
 -- Create index
 
-CREATE INDEX idx_transactions_clean_event_time
+CREATE INDEX IF NOT EXISTS idx_transactions_clean_event_time
     ON transactions_clean(event_time);
 
-CREATE INDEX idx_transactions_clean_user_id
+CREATE INDEX IF NOT EXISTS idx_transactions_clean_user_id
     ON transactions_clean(user_id);
 
-CREATE INDEX idx_transactions_clean_is_late
+CREATE INDEX IF NOT EXISTS idx_transactions_clean_is_late
     ON transactions_clean(is_late);
+
+COMMIT;
